24e74f3efc21d71268ecb4cc2aa8933daeb00bbf
Lialichev <lyalichevv98@gmail.com>
2020-09-22 20:55:40 +0300

    feature/set-style-for-world-situation-section

--- src/pages/General/components/WorldSituation/index.tsx
+++ src/pages/General/components/WorldSituation/index.tsx
@@ -6,7 +6,21 @@ const WorldSituation: React.FC = (): JSX.Element => {
 
     return (
         <div className="world-situation">
-
+            <div className="container">
+                <div className="world-situation__inner">
+                    <div className="world-situation__title">
+                        World situation
+                    </div>
+                    <div className="world-situation__boxes">
+                        <div className="world-situation__card">181 237</div>
+                        <div className="world-situation__card world-situation__card--50">181 237</div>
+                        <div className="world-situation__card world-situation__card--50">181 237</div>
+                        <div className="world-situation__card world-situation__card--33">181 237</div>
+                        <div className="world-situation__card world-situation__card--33">181 237</div>
+                        <div className="world-situation__card world-situation__card--33">181 237</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
